55f9f91a589f5cd74bf38997a38f482d6c92ccdf
Gregor Holzmann <debrisplease@gmail.com>
2017-05-31 11:26:41 -0700

    initial commit - opensources coffeebot

---
+++ coffee_bot.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+"""Slack bot for fresh pots of coffee!"""
+
+from scapy.all import *
+import requests
+import constants
+SLACK_API_URL = 'https://slack.com/api/chat.postMessage'
+
+def send_message():
+    """Posts to Slack API with token auth and message payload"""
+    data = {
+        "token": constants.SLACK_TOKEN,
+        "channel": constants.CHANNEL_ID,
+        "text": ":coffee: Fresh pot of coffee in the kitchen! :coffee:",
+        "as_user": "true"
+    }
+    requests.post(SLACK_API_URL, data)
+
+def arp_display(pkt):
+    """Sniffs network packets for the dash button press"""
+    if pkt[ARP].op == 1:
+        if pkt[ARP].psrc == '0.0.0.0':
+            if pkt[ARP].hwsrc == constants.MAC_ADDRESS:
+                print 'butts'
+                # send_message()
+
+
+print sniff(prn=arp_display, filter="arp", store=0)
---
+++ constants-example.py
@@ -0,0 +1,9 @@
+"""
+Example constants file:
+
+Add your slack token, channel id, and MAC address of your dash button here,
+then rename this file to 'constants.py`
+"""
+SLACK_TOKEN = "SLACK TOKEN HERE"
+CHANNEL_ID = "CHANNEL ID HERE"
+MAC_ADDRESS = "MAC ADDRESS HERE"
